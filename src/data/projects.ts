84fe0dedf0a4f212253da71254e81d724b2f7552
supershuaifeng <limchinfeng@gmail.com>
2024-07-12 22:47:16 +0800

    fix: Image to img

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,6 +1,7 @@
 export const projects = [
   {
     title: "Automated Testing - Laravel Dusk & Selenium",
+    image: "image/index/automated-testing.svg",
     description: "Dived into PHP automated testing and whipped up some simple docs on setting up and using Laravel Dusk and Selenium. Check out the setup and links for two test cases!",
     tags: ["Laravel", "php", "Automated Testing"],
     html:  `<a class="underline cursor-pointer hover:text-light-hover dark:hover:text-dark-hover transition text-title" href="https://docs.google.com/document/d/1XTw3KhSLihczlayC-6ZxDfXLleHW0ysLHNhK0AAoAyk/edit?usp=sharing" target="_blank">Laravel Dusk</a> 
@@ -9,37 +10,44 @@ export const projects = [
   },
   {
     title: "HackEclipse",
+    image: "image/index/HackEclipse.svg",
     description: `Built for a GDSC USM hackathon, this website allows participants register and submit projects, and allowed judges to evaluate them. The event got canceled, but hey, we finished the website!`,
     tags: ["Fullstack", "Astro", "Tailwind CSS", "Javascript", "Firebase"],
   },
   {
     title: "Internify",
+    image: "image/index/internify.svg",
     description: "A fullstack project for school. Developed a dynamic internship management platform where users can explore internship opportunities, showcase their personal projects, and receive tailored recommendations based on their preferences.",
     tags: ["Fullstack", "NextJS", "React", "Typescript", "MongoDB", "Tailwind CSS"],
   },
   {
     title: "Laravel Food Ordering E-commerce",
+    image: "image/index/e-commerce.svg",
     description: "An e-commerce project developed to learn the Laravel framework. It allows users to order food, manage a shopping cart, and make payments with automated receipt generation, while administrators can track and update order statuses in real-time.",
     tags: ["Fullstack", "Laravel", "JQuery", "MySQL", "Tailwind CSS"],
   },
   {
     title: "Static Website - Therapist Portfolio",
+    image: "image/index/therapist.svg",
     description: "My very first responsive static portfolio website using Next.js and React.js. Designed the UI in Figma. Simple, but it`s my first website and I'm proud of it!",
     tags: ["Frontend", "React", "Framer Motion"],
     link: "https://limkimseah.com/"
   },
   {
     title: "Fullstack Hackathon Project - Trustay",
+    image: "image/index/trustay.svg",
     description: "Developed a prototype enabling users to seamlessly rent properties while giving administrators a comprehensive reservation overview.",
     tags: ["Fullstack", "NextJS", "React", "Typescript", "MongoDB", "Tailwind CSS"],
   },
   {
     title: "Depression Tweet Data Collection",
+    image: "image/index/research.svg",
     description: "Worked on a research project on Depression Tweet Data with Dr. Jasy Liew Suet Yan. Used Twitter API and snscrape to collect loads of tweets. Spent time cleaning and validating the data to make sure it was spot on.",
     tags: ["Python", "Data Collection", "Data Cleaning", "Data Validation"],
   },
   {
     title: "PIXEL2022",
+    image: "image/index/pixelusm.svg",
     description: "PIXEL is the annual tech showcase at USM. It is built with database of all student projects and a dashboard for judges to evaluate them.",
     tags: ["Svelte", "Firebase", "Tailwind CSS"],
     link: "https://pixel2022.cssocietyusm.com/"
