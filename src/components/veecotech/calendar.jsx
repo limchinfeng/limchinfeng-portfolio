5b9fdd352891f694026fe638ac49cf1e9c43754e
supershuaifeng <limchinfeng@gmail.com>
2024-09-22 21:30:38 +0800

    feat: veecotech availability calendar

---
+++ src/components/veecotech/calendar.jsx
@@ -0,0 +1,57 @@
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import { availability } from "@/data/availability.ts";
+
+
+const mapAvailabilityToEvents = (availability) => {
+  return availability.map((item) => {
+    return {
+      title: item.title,
+      start: `${item.date}T${item.startTime}:00`, // ISO 8601 format
+      end: `${item.date}T${item.endTime}:00`,
+      allDay: false  // Set to false since it's not an all-day event
+    };
+  });
+};
+
+function Calendar() {
+
+  const handleEventClick = (clickInfo) => {
+    const { title, start, end } = clickInfo.event;
+
+    // Format start and end times to 24-hour format
+    const startFormatted = start.toLocaleString("en-GB", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
+
+    const endFormatted = end.toLocaleString("en-GB", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    });
+
+    window.alert(`Event: ${title}\nStart: ${startFormatted}\nEnd: ${endFormatted}`);
+  };
+
+  return (
+    <Fullcalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      initialView={"dayGridMonth"}
+      headerToolbar={{
+        start: "today prev,next", 
+        center: "title",
+        end: "dayGridMonth,timeGridWeek,timeGridDay", 
+      }}
+      events={mapAvailabilityToEvents(availability)} // Pass events to FullCalendar
+      eventClick={handleEventClick}
+    />
+  );
+}
+
+export default Calendar;
---
+++ src/data/availability.ts
@@ -0,0 +1,65 @@
+export const availability = [
+  {
+    "title": "Monday Blue - Day 1",
+    "date": "2024-10-01",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+  {
+    "title": "Day 2",
+    "date": "2024-10-02",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+  {
+    "title": "Day 3",
+    "date": "2024-10-03",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+  {
+    "title": "Day 4",
+    "date": "2024-10-04",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+  {
+    "title": "Day 5",
+    "date": "2024-10-07",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+  {
+    "title": "Day 6",
+    "date": "2024-10-08",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+  {
+    "title": "Day 7",
+    "date": "2024-10-09",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+  {
+    "title": "Day 8",
+    "date": "2024-10-10",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+  {
+    "title": "Day 9",
+    "date": "2024-10-11",
+    "startTime": "08:30",
+    "endTime": "12:30"
+  },
+];
+
+export const announcements = [
+  {
+    "title": "First Working Day",
+    "description": "My first day of my part time work will be on 1st October 2024.",
+    "type": "info",
+    "date": "2024-10-01",
+  },
+];
