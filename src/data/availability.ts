b0789928c7326f8c6aa2537d290352acce72db67
supershuaifeng <limchinfeng@gmail.com>
2024-10-13 20:59:56 +0800

    fix: update announcement

--- src/data/availability.ts
+++ src/data/availability.ts
@@ -114,10 +114,10 @@ export const availability = [
 ];
 
 export const announcements = [
-  // {
-  //   "title": "First Working Day",
-  //   "description": "My first day of my part time work will be on 1st October 2024.",
-  //   "type": "info",
-  //   "date": "2024-10-01",
-  // },
+  {
+    "title": "",
+    "description": "",
+    "type": "info",
+    "date": "",
+  },
 ];
